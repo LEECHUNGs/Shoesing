30154958318f94ab023638944b527a44d9ad479f
JUHAHAH <108599647+JUHAHAH@users.noreply.github.com>
2024-04-24 14:25:10 +0900

    SHOESING FIRST UPLOAD

---
+++ src/main/java/kr/co/shoesing/ShoesingApplication.java
@@ -0,0 +1,13 @@
+package kr.co.shoesing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesingApplication.class, args);
+	}
+
+}
