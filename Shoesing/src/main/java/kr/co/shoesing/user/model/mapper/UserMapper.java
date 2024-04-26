a9a46d30611fc39513eb92d5e590e18739c2d7c5
minnily <156267800+minnily@users.noreply.github.com>
2024-04-26 12:48:48 +0900

    4.26

---
+++ Shoesing/src/main/java/kr/co/shoesing/user/model/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package kr.co.shoesing.user.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.shoesing.user.model.dto.User;
+
+@Mapper
+public interface UserMapper {
+
+	/**
+	 * 회원 로그인
+	 * 
+	 * @return User
+	 */
+	User login();
+
+	/**
+	 * 회원 회원가입
+	 * 
+	 * @param inputUser
+	 */
+	void signup(User inputUser);
+
+}
