ea8d5cb2a2b3ff047c0f8d630f05310e8611696d
minnily <dlalsrud20142905@gmail.com>
2024-04-30 16:12:23 +0900

    444

---
+++ Shoesing/src/main/resources/static/js/main.js
@@ -0,0 +1,81 @@
+document.documentElement.style.setProperty(
+  '--scrollbar-width',
+  window.innerWidth - document.documentElement.clientWidth + 'px'
+);
+
+// 스크롤 시 헤더 숨김
+document.addEventListener('DOMContentLoaded', () => {
+  el_autohide = document.querySelector('.header-main');
+
+  if (el_autohide) {
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function () {
+      let scroll_top = window.scrollY;
+      if (scroll_top < last_scroll_top) {
+        el_autohide.classList.remove('scrolled-down');
+        el_autohide.classList.add('scrolled-up');
+      } else {
+        el_autohide.classList.remove('scrolled-up');
+        el_autohide.classList.add('scrolled-down');
+      }
+      last_scroll_top = scroll_top;
+    });
+  }
+});
+
+
+// 로그인폼, 로그인 id, 로그인 pw  얻어오기
+const loginForm = document.querySelector("#loginForm");
+
+const loginId= document.querySelector("#loginForm input[name='userId']");
+const loginPw = document.querySelector("#loginForm input[name='userPw']");
+
+
+// 쿠키 내용
+const getCookie =(key) => {
+  const cookies = document.cookie;
+  const cookieList = cookies.split(";").map(el => el.split("="));
+  
+  const obj ={};
+
+  for(let t=0; i<cookieList.length; i++){
+    const k = cookieList[i][0];
+    const v = cookieList[i][1];
+    obj[k] = v; 
+  }
+  return obj[key];
+};
+
+//Id 저장
+if(loginId != null){
+  const saveId = getCookie("saveId");
+
+  if(saveId != undefined){
+    loginId.value =saveId;
+    document.querySelector("input[name='saveId']").checked =true;
+  }
+};
+
+// 로그인 유효성 검사
+if(loginForm != null){
+  loginForm.addEventListener("submit", e =>{
+
+    if(loginId.value.trim().length ===  0){
+        alert("아이디를 입력해주세요");
+        e.preventDefault();
+        loginId.focus();
+        return;
+    }
+
+    if(loginPw.value.trim().length ===  0){
+      alert("비밀번호를 입력해주세요");
+      e.preventDefault();
+      loginPw.focus();
+      return;
+    }
+  });
+}
+
+
+
+
---
+++ Shoesing/src/test/java/kr/co/shoesing/ShoesingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.shoesing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
