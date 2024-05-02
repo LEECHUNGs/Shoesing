54988741ce29c36a7366398294b837a467393a5b
LEECHUNGs <ragnacrimson@naver.com>
2024-05-02 17:36:15 +0900

    wishList

--- Shoesing/src/main/java/kr/co/shoesing/wishList/model/mapper/WishListMapper.java
+++ Shoesing/src/main/java/kr/co/shoesing/wishList/model/mapper/WishListMapper.java
@@ -1,5 +1,45 @@
 package kr.co.shoesing.wishList.model.mapper;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
+
+import kr.co.shoesing.item.model.dto.Item;
+
+@Mapper
 public interface WishListMapper {
+
+	/** DB에 해당 위시리스트가 존재하는지 확인
+	 * @param map
+	 * @return check
+	 */
+	int check(Map<String, Object> map);
+	
+	/** 위시리스트 추가
+	 * @param map
+	 * @return result
+	 */
+	int insert(Map<String, Object> map);
+
+	/** 유저의 위시리스트 개수 불러오기
+	 * @param userNo 
+	 * @return
+	 */
+	int getListCount(int userNo);
+
+	/** 유저의 위시리스트 불러오기
+	 * @param userNo
+	 * @param rowBounds
+	 * @return
+	 */
+	List<Item> selectAll(int userNo, RowBounds rowBounds);
+
+	/** 선택된 위시리스트 삭제
+	 * @param itemNoList
+	 * @return
+	 */
+	int delete(List<String> itemNoList);
 
 }
--- Shoesing/src/main/resources/static/js/item/itemDetail.js
+++ Shoesing/src/main/resources/static/js/item/itemDetail.js
@@ -1,11 +1,25 @@
 // 비동기로 wishList에 상품 추가
 document.getElementById("wishListBtn").addEventListener("click", () => {
 
-    fetch("wishList", {
+    if(loginUser == null) {
+        alert("로그인 후 이용해 주세요!!");
+        return;
+    }
+
+    fetch("/wishList/manage", {
         method : "post",
         headers : {"Content-type" : "application/json"},
         body : JSON.stringify(item)
     })
     .then(resp => resp.text())
-    .then();
+    .then(result => {
+
+        if(result > 0) {
+            alert("상품이 등록되었습니다.")
+
+        } else {
+            alert("이미 등록된 상품입니다.")
+        }
+        
+    });
 });
