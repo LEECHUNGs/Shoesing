debe2a02ef428eebe399487dabf699f4db54c2f5
JUHAHAH <108599647+JUHAHAH@users.noreply.github.com>
2024-05-07 17:31:20 +0900

    내일은 열심히 해야지
    회원가입 끝, 유저 키 수정

--- Shoesing/src/main/java/kr/co/shoesing/item/controller/ItemController.java
+++ Shoesing/src/main/java/kr/co/shoesing/item/controller/ItemController.java
@@ -1,16 +1,13 @@
 package kr.co.shoesing.item.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.shoesing.item.model.dto.Item;
 import kr.co.shoesing.item.model.service.ItemService;
@@ -22,58 +19,46 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("item")
 @RequiredArgsConstructor
 public class ItemController {
-	
+
 	private final ItemService service;
-	
-	/** 상품 목록 페이지 뷰
+
+	/**
+	 * 상품 목록 페이지 뷰
+	 * 
 	 * @return
 	 */
 	@GetMapping("info")
 	public String getMethodName() {
 		return "pages/item";
 	}
-	
-	/** 상품 목록 불러오기
+
+	/**
+	 * 상품 목록 불러오기
+	 * 
 	 * @param sortNo
 	 * @return
 	 */
 	@ResponseBody
 	@GetMapping("itemList")
-	public Map<String, Object> itemList(
-					@RequestParam("sortNo") int sortNo,
-					@RequestParam(value = "cp", required = false, defaultValue = "1") int cp) {
-		
+	public Map<String, Object> itemList(@RequestParam("sortNo") int sortNo,
+			@RequestParam(value = "cp", required = false, defaultValue = "1") int cp) {
+
 		return service.selectAll(sortNo, cp);
 	}
-	
-	/** 상품 상세 페이지 뷰
+
+	/**
+	 * 상품 상세 페이지 뷰
+	 * 
 	 * @param itemNo
 	 * @param model
 	 * @return
 	 */
 	@GetMapping("detail")
-	public String detail(@RequestParam("itemNo") int itemNo, 
-						 Model model) {
-		
+	public String detail(@RequestParam("itemNo") int itemNo, Model model) {
+
 		Item item = service.selectOne(itemNo);
 		model.addAttribute("item", item);
 		return "pages/itemDetail";
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
